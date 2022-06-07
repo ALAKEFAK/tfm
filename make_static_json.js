aea2ee63a904abd1e0d8126a756564836802a55e
Moty Rosilio <alakefak@gmail.com>
2022-06-07 16:05:44 +0300

    changing getWaitingForTimeout from 500 to 2500

--- make_static_json.js
+++ make_static_json.js
@@ -54,7 +54,7 @@ function getWaitingForTimeout() {
   if (process.env.WAITING_FOR_TIMEOUT) {
     return Number(process.env.WAITING_FOR_TIMEOUT);
   }
-  return 500;
+  return 2500;
 }
 
 function getLogLength() {
