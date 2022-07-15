bb5dfbd6916f5964109d0e07e4c447e3e297e91c
Dan Bjork <dan.bjoerk@gmail.com>
2022-07-15 19:01:54 +0200

    Builder: display 7 tags instead + update image asset

--- src/milestones/Builder.ts
+++ src/milestones/Builder.ts
@@ -4,7 +4,7 @@ import {Tags} from '../cards/Tags';
 
 export class Builder implements IMilestone {
     public name: string = 'Builder';
-    public description: string = 'Having at least 8 building tags in play'
+    public description: string = 'Having at least 7 building tags in play'
     public getScore(player: Player): number {
       return player.getTagCount(Tags.BUILDING, false, true, true);
     }
