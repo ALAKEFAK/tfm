7ff0a2ab973fa9ef2773fd8c7dd4c808fb24456c
Stefan <stefan.foerstel@infoteam.de>
2022-07-16 22:17:32 +0200

    fix: Add Incinerator

--- src/cards/league/LeagueCardManifest.ts
+++ src/cards/league/LeagueCardManifest.ts
@@ -3,11 +3,13 @@ import {GameModule} from '../../GameModule';
 import {CardManifest} from '../CardManifest';
 import {SaunaTicket} from './league_project/SaunaTicket';
 import {MarsHeavyIndustry} from './league_project/MarsHeavyIndustry';
+import {Incinerator} from './league_project/Incinerator';
 
 export const LEAGUE_CARD_MANIFEST = new CardManifest({
   module: GameModule.League,
   projectCards: [
-    {cardName: CardName.SAUNA_TICKET, Factory: SaunaTicket},
+    {cardName: CardName.INCINERATOR, Factory: Incinerator},
     {cardName: CardName.MARS_HEAVY_INDUSTRY, Factory: MarsHeavyIndustry},
+    {cardName: CardName.SAUNA_TICKET, Factory: SaunaTicket},
   ],
 });
