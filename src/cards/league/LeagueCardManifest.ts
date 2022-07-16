56f7493807dc6732a1950c38e90c1e21f27faa7d
Stefan <stefan.foerstel@infoteam.de>
2022-07-16 22:51:14 +0200

    feat: Add RecycledProjects

--- src/cards/league/LeagueCardManifest.ts
+++ src/cards/league/LeagueCardManifest.ts
@@ -4,12 +4,14 @@ import {CardManifest} from '../CardManifest';
 import {SaunaTicket} from './league_project/SaunaTicket';
 import {MarsHeavyIndustry} from './league_project/MarsHeavyIndustry';
 import {Incinerator} from './league_project/Incinerator';
+import {RecycledProjects} from './league_project/RecycledProjects';
 
 export const LEAGUE_CARD_MANIFEST = new CardManifest({
   module: GameModule.League,
   projectCards: [
     {cardName: CardName.INCINERATOR, Factory: Incinerator},
     {cardName: CardName.MARS_HEAVY_INDUSTRY, Factory: MarsHeavyIndustry},
+    {cardName: CardName.RECYCLED_PROJECTS, Factory: RecycledProjects},
     {cardName: CardName.SAUNA_TICKET, Factory: SaunaTicket},
   ],
 });
---
+++ src/cards/league/league_project/RecycledProjects.ts
@@ -0,0 +1,46 @@
+import {IProjectCard} from '../../IProjectCard';
+import {Card} from '../../Card';
+import {CardType} from '../../CardType';
+import {Player} from '../../../Player';
+import {CardName} from '../../../CardName';
+import {CardRenderer} from '../../render/CardRenderer';
+import {Tags} from '../../Tags';
+import {CardRequirements} from '../../CardRequirements';
+
+export class RecycledProjects extends Card implements IProjectCard {
+  constructor() {
+    super({
+      cardType: CardType.ACTIVE,
+      name: CardName.RECYCLED_PROJECTS,
+      cost: 19,
+      tags: [Tags.SCIENCE, Tags.BUILDING, Tags.SPACE],
+
+      requirements: CardRequirements.builder((b) => b.tag(Tags.SCIENCE, 4)),
+      metadata: {
+        cardNumber: 'L304',
+        renderData: CardRenderer.builder((b) => {
+          b.effect(undefined, (eb) => {
+            eb.building(1).played.startEffect.steel(1);
+          }).br;
+          b.effect(undefined, (eb) => {
+            eb.space().played.startEffect.titanium(1);
+          }).br;
+        }),
+        description: 'After playing a building or space tag, including these, receive a steel or titanium resource respectively.',
+        victoryPoints: 1,
+      },
+    });
+  }
+
+  public play() {
+    return undefined;
+  }
+
+  public onCardPlayed(player: Player, card: IProjectCard): void {
+    const numBuildingTags = card.tags.filter((tag) => tag === Tags.BUILDING).length;
+    const numSpaceTags = card.tags.filter((tag) => tag === Tags.SPACE).length;
+    if (numBuildingTags > 0) player.steel += numBuildingTags;
+    if (numSpaceTags > 0) player.titanium += numSpaceTags;
+    return undefined;
+  }
+}
