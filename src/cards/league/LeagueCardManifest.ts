c86bece8a1d6a544fae28eac9d67d20df9b8104a
Stefan <stefan.foerstel@infoteam.de>
2022-07-16 22:02:49 +0200

    feat: Add MarsHeavyIndustry

--- src/cards/league/LeagueCardManifest.ts
+++ src/cards/league/LeagueCardManifest.ts
@@ -2,10 +2,12 @@ import {CardName} from '../../CardName';
 import {GameModule} from '../../GameModule';
 import {CardManifest} from '../CardManifest';
 import {SaunaTicket} from './league_project/SaunaTicket';
+import {MarsHeavyIndustry} from './league_project/MarsHeavyIndustry';
 
 export const LEAGUE_CARD_MANIFEST = new CardManifest({
   module: GameModule.League,
   projectCards: [
     {cardName: CardName.SAUNA_TICKET, Factory: SaunaTicket},
+    {cardName: CardName.MARS_HEAVY_INDUSTRY, Factory: MarsHeavyIndustry},
   ],
 });
