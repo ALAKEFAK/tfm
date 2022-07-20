048c7596c386b389aa0ddc25a2071a5c1df6129f
Stefan <stefan.foerstel@infoteam.de>
2022-07-20 13:16:43 +0200

    fix: Change Smelting Plant: 2 O2, 4 steel (original card) + 1 building card draw

--- src/cards/rebalanced/rebalanced_prelude/SmeltingPlantRebalanced.ts
+++ src/cards/rebalanced/rebalanced_prelude/SmeltingPlantRebalanced.ts
@@ -13,14 +13,20 @@ export class SmeltingPlantRebalanced extends PreludeCard {
       metadata: {
         cardNumber: 'P30',
         renderData: CardRenderer.builder((b) => {
-          b.oxygen(3);
+          b.oxygen(2).br;
+          b.steel(4).br;
+          b.effect(undefined, (eb) => {
+            eb.empty().startAction.cards(1).secondaryTag(Tags.BUILDING);
+          });
         }),
-        description: 'Raise oxygen 3 steps.',
+        description: 'Raise oxygen 2 steps. Gain 4 steel. Draw 1 building card.',
       },
     });
   }
   public play(player: Player) {
-    player.game.increaseOxygenLevel(player, 1);
-    return player.game.increaseOxygenLevel(player, 2);
+    player.steel += 4;
+    player.game.increaseOxygenLevel(player, 2);
+    player.drawCard(1, {tag: Tags.BUILDING});
+    return undefined;
   }
 }
