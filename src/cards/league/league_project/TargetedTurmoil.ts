8e9c3f78282dc25320a4617243dc80ee3978c925
Stefan <stefan.foerstel@infoteam.de>
2022-07-27 13:00:33 +0200

    fix: Add canPlay method to TargetedTurmoil to fix bug that

--- src/cards/league/league_project/TargetedTurmoil.ts
+++ src/cards/league/league_project/TargetedTurmoil.ts
@@ -38,18 +38,28 @@ export class TargetedTurmoil extends Card implements IProjectCard {
     });
   }
 
+  public canPlay(player: Player) {
+    return super.canPlay(player) && (
+      (player.game.getTemperature() > MIN_TEMPERATURE && player.game.getTemperature() < MAX_TEMPERATURE) ||
+      (player.game.getOxygenLevel() > MIN_OXYGEN_LEVEL && player.game.getOxygenLevel() < MAX_OXYGEN_LEVEL) ||
+      (player.game.board.getOceansOnBoard() > 0 && player.game.board.getOceansOnBoard() < MAX_OCEAN_TILES) ||
+      (player.game.gameOptions.venusNextExtension &&
+        player.game.getVenusScaleLevel() > MIN_VENUS_SCALE && player.game.getVenusScaleLevel() < MAX_VENUS_SCALE)
+    );
+  }
+
   public play(player: Player) {
     player.decreaseTerraformRating();
     const orOptions = new OrOptions();
 
-    if (player.game.getTemperature() !== MIN_TEMPERATURE && player.game.getTemperature() < MAX_TEMPERATURE) {
+    if (player.game.getTemperature() > MIN_TEMPERATURE && player.game.getTemperature() < MAX_TEMPERATURE) {
       orOptions.options.push(new SelectOption('Decrease Temperature', 'Decrease Temperature', () => {
         player.game.increaseTemperature(player.game.getPlayers()[0], -1);
         return undefined;
       }));
     }
 
-    if (player.game.getOxygenLevel() !== MIN_OXYGEN_LEVEL && player.game.getOxygenLevel() !== MAX_OXYGEN_LEVEL) {
+    if (player.game.getOxygenLevel() > MIN_OXYGEN_LEVEL && player.game.getOxygenLevel() < MAX_OXYGEN_LEVEL) {
       orOptions.options.push(new SelectOption('Decrease Oxygen level', 'Decrease O2', () => {
         player.game.increaseOxygenLevel(player.game.getPlayers()[0], -1);
         return undefined;
@@ -57,7 +67,7 @@ export class TargetedTurmoil extends Card implements IProjectCard {
     }
 
     const oceansPlaced = player.game.board.getOceansOnBoard();
-    if (oceansPlaced > 0 && oceansPlaced !== MAX_OCEAN_TILES) {
+    if (oceansPlaced > 0 && oceansPlaced < MAX_OCEAN_TILES) {
       orOptions.options.push(new SelectOption('Remove Ocean', 'Remove Ocean', () => {
         player.game.defer(new RemoveOceanTile(player, 'Targeted Turmoil - Remove an Ocean tile from the board'));
         return undefined;
@@ -65,14 +75,15 @@ export class TargetedTurmoil extends Card implements IProjectCard {
     }
 
     if (player.game.gameOptions.venusNextExtension &&
-      player.game.getVenusScaleLevel() !== MIN_VENUS_SCALE &&
-      player.game.getVenusScaleLevel() !== MAX_VENUS_SCALE) {
+      player.game.getVenusScaleLevel() > MIN_VENUS_SCALE &&
+      player.game.getVenusScaleLevel() < MAX_VENUS_SCALE) {
       orOptions.options.push(new SelectOption('Decrease Venus level', 'Decrease Venus', () => {
         player.game.increaseVenusScaleLevel(player.game.getPlayers()[0], -1);
         return undefined;
       }));
     }
 
+    if (orOptions.options.length === 0) return undefined;
     if (orOptions.options.length === 1) return orOptions.options[0].cb();
     return orOptions;
   }
