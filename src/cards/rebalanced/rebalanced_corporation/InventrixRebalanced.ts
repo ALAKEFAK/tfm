dc50cf972bafa5c8abe3fb4e4bf118075fa1a428
Stefan <stefan.foerstel@infoteam.de>
2022-08-27 16:43:23 +0200

    feat: Change ReqBonus back to 2, add cutting edge effect.

--- src/cards/rebalanced/rebalanced_corporation/InventrixRebalanced.ts
+++ src/cards/rebalanced/rebalanced_corporation/InventrixRebalanced.ts
@@ -5,6 +5,9 @@ import {Player} from '../../../Player';
 import {CardName} from '../../../CardName';
 import {CardType} from '../../CardType';
 import {CardRenderer} from '../../render/CardRenderer';
+import {AltSecondaryTag} from '../../render/CardRenderItem';
+import {Size} from '../../render/Size';
+import {IProjectCard} from '../../IProjectCard';
 
 export class InventrixRebalanced extends Card implements CorporationCard {
   constructor() {
@@ -17,26 +20,37 @@ export class InventrixRebalanced extends Card implements CorporationCard {
 
       metadata: {
         cardNumber: 'R43',
-        description: 'As your first action in the game, draw 3 cards. Start with 45 M€.',
+        description: 'As first action, draw 3 cards.',
         renderData: CardRenderer.builder((b) => {
-          b.br;
           b.megacredits(45).nbsp.cards(3);
           b.corpBox('effect', (ce) => {
-            ce.effect('Your temperature, oxygen, ocean, and Venus requirements are +4 or -4 steps, your choice in each case.', (eb) => {
-              eb.plate('Global requirements').startEffect.text('+/- 4');
+            ce.vSpace(Size.LARGE);
+            ce.effect('Cards with a req. cost 2 M€ less.', (eb) => {
+              eb.cards(1).secondaryTag(AltSecondaryTag.REQ).startEffect.megacredits(-2);
+            });
+            ce.effect('Your temperature, oxygen, ocean, and Venus requirements are +/-2 steps.', (eb) => {
+              eb.plate('Global requirements').startEffect.text('+/- 2');
             });
           });
         }),
       },
     });
   }
+
   public initialAction(player: Player) {
     player.drawCard(3);
     return undefined;
   }
-  public getRequirementBonus(): number {
-    return 4;
+
+  public getCardDiscount(_player: Player, card: IProjectCard) {
+    if (card.requirements !== undefined) return 2;
+    return 0;
   }
+
+  public getRequirementBonus(): number {
+    return 2;
+  }
+
   public play() {
     return undefined;
   }
