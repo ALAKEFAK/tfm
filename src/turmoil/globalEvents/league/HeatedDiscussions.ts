419594298cfc090c0a3b72f3fb0f332b82d84d46
Stefan <stefan.foerstel@infoteam.de>
2022-08-29 14:34:14 +0200

    fix: Change to 'Lose 1 M€ for each science tag. Gain 1 heat per heat production (max 5) and 2 heat per influence.'

--- src/turmoil/globalEvents/league/HeatedDiscussions.ts
+++ src/turmoil/globalEvents/league/HeatedDiscussions.ts
@@ -8,16 +8,13 @@ import {Tags} from '../../../cards/Tags';
 
 export class HeatedDiscussions implements IGlobalEvent {
     public name = GlobalEventName.HEATED_DISCUSSIONS;
-    public description = 'Lose 2 M€ for each science tag (max 5, then reduced by influence). Gain 1 heat per heat production (max 5).';
+    public description = 'Lose 1 M€ for each science tag. Gain 1 heat per heat production (max 5) and 2 heat per influence.';
     public revealedDelegate = PartyName.SCIENTISTS;
     public currentDelegate = PartyName.KELVINISTS;
     public resolve(game: Game, turmoil: Turmoil) {
       game.getPlayers().forEach((player) => {
-        const amount = Math.min(5, player.getTagCount(Tags.SCIENCE, false, false)) - turmoil.getPlayerInfluence(player);
-        if (amount > 0) {
-          player.addResource(Resources.MEGACREDITS, -2 * amount, {log: true, from: this.name});
-        }
-        player.addResource(Resources.HEAT, Math.min(5, player.getProduction(Resources.HEAT)), {log: true, from: this.name});
+        player.addResource(Resources.MEGACREDITS, -player.getTagCount(Tags.SCIENCE, false, false), {log: true, from: this.name});
+        player.addResource(Resources.HEAT, Math.min(5, player.getProduction(Resources.HEAT)) + 2 * turmoil.getPlayerInfluence(player), {log: true, from: this.name});
       });
     }
 }
