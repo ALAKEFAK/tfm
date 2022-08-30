d25868d084fab3a7c0f322cbac63e00a2931ebab
Stefan <stefan.foerstel@infoteam.de>
2022-08-30 14:57:48 +0200

    fix: Add produce method to SoilFactory and SoilFactoryRebalanced to be useable with RoboticWorkforce

--- src/cards/base/SoilFactory.ts
+++ src/cards/base/SoilFactory.ts
@@ -30,15 +30,22 @@ export class SoilFactory extends Card implements IProjectCard {
       },
     });
   }
+
   public canPlay(player: Player): boolean {
     return player.getProduction(Resources.ENERGY) >= 1;
   }
+
   public play(player: Player) {
-    player.addProduction(Resources.ENERGY, -1);
-    player.addProduction(Resources.PLANTS, 1);
+    this.produce(player);
     return undefined;
   }
+
   public getVictoryPoints() {
     return 1;
+  }
+
+  public produce(player: Player) {
+    player.addProduction(Resources.ENERGY, -1);
+    player.addProduction(Resources.PLANTS, 1);
   }
 }
