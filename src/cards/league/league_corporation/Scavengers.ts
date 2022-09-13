a2152dd59051cbc6cb009e9d2e2e619afd940c0d
Stefan <stefan.foerstel@infoteam.de>
2022-09-13 10:37:03 +0200

    fix: Remove production from Scavengers

--- src/cards/league/league_corporation/Scavengers.ts
+++ src/cards/league/league_corporation/Scavengers.ts
@@ -7,8 +7,8 @@ import {CorporationCard} from '../../corporation/CorporationCard';
 import {TileType} from '../../../TileType';
 import {ISpace} from '../../../boards/ISpace';
 import {SelectSpace} from '../../../inputs/SelectSpace';
-import {Resources} from '../../../Resources';
-import {SelectScavengersStartingProd} from '../../../deferredActions/SelectScavengersStartingProd';
+// import {Resources} from '../../../Resources';
+// import {SelectScavengersStartingProd} from '../../../deferredActions/SelectScavengersStartingProd';
 
 export class Scavengers extends Card implements CorporationCard {
   // burner
@@ -22,9 +22,11 @@ export class Scavengers extends Card implements CorporationCard {
 
       metadata: {
         cardNumber: 'L421',
-        description: 'You start with 46 M€ and 1 of every other resource. Choose 2 different production types. As your first action, place the crashed space ship tile on mars.',
+        // description: 'You start with 46 M€ and 1 of every other resource. Choose 2 different production types. As your first action, place the crashed space ship tile on mars.',
+        description: 'You start with 46 M€ and 1 of every other resource. As your first action, place the crashed space ship tile on mars.',
         renderData: CardRenderer.builder((b) => {
-          b.megacredits(46).production((pb) => pb.megacredits(-3).wild(2)).wild(1).tile(TileType.SCAVENGERS, true);
+          b.megacredits(46).wild(1).tile(TileType.SCAVENGERS, true);
+          // b.megacredits(46).production((pb) => pb.megacredits(-3).wild(2)).wild(1).tile(TileType.SCAVENGERS, true);
           b.corpBox('effect', (ce) => {
             ce.effect('When you place a tile on mars, gain an additional bonus of each type printed on the tile.', (eb) => {
               eb.text('n').wild(1).startEffect.text('(n+1)').wild(1);
@@ -44,13 +46,13 @@ export class Scavengers extends Card implements CorporationCard {
   }
 
   public play(player: Player) {
-    player.addProduction(Resources.MEGACREDITS, -3);
+    // player.addProduction(Resources.MEGACREDITS, -3);
     player.steel++;
     player.titanium++;
     player.plants++;
     player.energy++;
     player.heat++;
-    player.game.defer(new SelectScavengersStartingProd(player));
+    // player.game.defer(new SelectScavengersStartingProd(player));
     return undefined;
   }
 }
