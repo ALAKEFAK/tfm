d6e87285f29b631e5414a1fa7b791d8c244fa1c6
Stefan <stefan.foerstel@infoteam.de>
2022-09-26 15:29:46 +0200

    fix: Change to +-3 params and -1 req cards

--- src/cards/rebalanced/rebalanced_corporation/InventrixRebalanced.ts
+++ src/cards/rebalanced/rebalanced_corporation/InventrixRebalanced.ts
@@ -16,20 +16,20 @@ export class InventrixRebalanced extends Card implements CorporationCard {
       name: CardName.INVENTRIX_REBALANCED,
       tags: [Tags.SCIENCE],
       initialActionText: 'Draw 3 cards',
-      startingMegaCredits: 40,
+      startingMegaCredits: 45,
 
       metadata: {
         cardNumber: 'R43',
         description: 'As first action, draw 3 cards.',
         renderData: CardRenderer.builder((b) => {
-          b.megacredits(40).nbsp.cards(3);
+          b.megacredits(45).nbsp.cards(3);
           b.corpBox('effect', (ce) => {
             ce.vSpace(Size.LARGE);
             ce.effect('Cards with a req. cost 2 M€ less.', (eb) => {
-              eb.cards(1).secondaryTag(AltSecondaryTag.REQ).startEffect.megacredits(-2);
+              eb.cards(1).secondaryTag(AltSecondaryTag.REQ).startEffect.megacredits(-1);
             });
-            ce.effect('Your temperature, oxygen, ocean, and Venus requirements are +/-2 steps.', (eb) => {
-              eb.plate('Global requirements').startEffect.text('+/- 2');
+            ce.effect('Your temperature, oxygen, ocean, and Venus requirements are +/-3 steps.', (eb) => {
+              eb.plate('Global requirements').startEffect.text('+/- 3');
             });
           });
         }),
@@ -43,12 +43,12 @@ export class InventrixRebalanced extends Card implements CorporationCard {
   }
 
   public getCardDiscount(_player: Player, card: IProjectCard) {
-    if (card.requirements !== undefined) return 2;
+    if (card.requirements !== undefined) return 1;
     return 0;
   }
 
   public getRequirementBonus(): number {
-    return 2;
+    return 3;
   }
 
   public play() {
