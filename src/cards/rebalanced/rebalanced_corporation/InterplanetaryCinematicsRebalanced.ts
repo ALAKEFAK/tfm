504b95958786de1bab9ba1842e156db4f53a7c70
Stefan <stefan.foerstel@infoteam.de>
2022-09-26 15:32:10 +0200

    fix: Change IC to 40MC and 12 steel starting res

--- src/cards/rebalanced/rebalanced_corporation/InterplanetaryCinematicsRebalanced.ts
+++ src/cards/rebalanced/rebalanced_corporation/InterplanetaryCinematicsRebalanced.ts
@@ -13,14 +13,14 @@ export class InterplanetaryCinematicsRebalanced extends Card implements Corporat
       cardType: CardType.CORPORATION,
       name: CardName.INTERPLANETARY_CINEMATICS_REBALANCED,
       tags: [Tags.BUILDING],
-      startingMegaCredits: 38,
+      startingMegaCredits: 40,
 
       metadata: {
         cardNumber: 'R19',
-        description: 'You start with 16 steel and 38 M€.',
+        description: 'You start with 40 M€ and 12 steel.',
         renderData: CardRenderer.builder((b) => {
           b.br.br.br;
-          b.megacredits(38).nbsp.steel(16).digit;
+          b.megacredits(40).nbsp.steel(12).digit;
           b.corpBox('effect', (ce) => {
             ce.effect('Each time you play an event, you gain 3 M€.', (eb) => {
               eb.event().played.startEffect.megacredits(3);
@@ -36,7 +36,7 @@ export class InterplanetaryCinematicsRebalanced extends Card implements Corporat
     }
   }
   public play(player: Player) {
-    player.steel = 16;
+    player.steel = 12;
     return undefined;
   }
 }
