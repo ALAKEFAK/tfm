19eed9bae21db8bb3eb365cb39268da738178d5d
Stefan <stefan.foerstel@infoteam.de>
2022-09-30 18:49:52 +0200

    fix: OuterRimUniversity was implemented wrongly. Had a mix of effects.

--- src/cards/league/league_project/OuterRimUniversity.ts
+++ src/cards/league/league_project/OuterRimUniversity.ts
@@ -3,33 +3,26 @@ import {IResourceCard} from '../../ICard';
 import {Card} from '../../Card';
 import {CardName} from '../../../CardName';
 import {CardType} from '../../CardType';
-import {ResourceType} from '../../../ResourceType';
 import {Tags} from '../../Tags';
 import {Player} from '../../../Player';
 import {OrOptions} from '../../../inputs/OrOptions';
 import {SelectOption} from '../../../inputs/SelectOption';
 import {CardRenderer} from '../../render/CardRenderer';
 import {DeferredAction} from '../../../deferredActions/DeferredAction';
+import {SelectCard} from '../../../inputs/SelectCard';
 
 export class OuterRimUniversity extends Card implements IProjectCard, IResourceCard {
-  public resourceCount = 0;
-
   constructor() {
     super({
       cardType: CardType.ACTIVE,
       name: CardName.OUTER_RIM_UNIVERSITY,
       tags: [Tags.JOVIAN, Tags.SPACE],
       cost: 10,
-      resourceType: ResourceType.ASTEROID,
 
       metadata: {
         cardNumber: 'L418',
         victoryPoints: 1,
-        // description: 'When you play a Jovian tag, including this, you may discard a card from hand to draw a card.',
         renderData: CardRenderer.builder((b) => {
-          // b.jovian().played.colon().asteroids(1).br;
-          // b.or().br;
-          // b.minus().asteroids(1).plus().cards(1);
           b.effect('When you play a Jovian tag, including this, you may discard a card from hand to draw a card.', (eb) => {
             eb.jovian().played.startEffect.minus().cards(1).nbsp.plus().cards(1);
           });
@@ -52,24 +45,28 @@ export class OuterRimUniversity extends Card implements IProjectCard, IResourceC
       player.game.defer(new DeferredAction(
         player,
         () => {
-          // Can't remove a resource
-          if (this.resourceCount === 0) {
-            player.addResourceTo(this, 1);
+          // No card to discard
+          if (player.cardsInHand.length === 0) {
             return undefined;
           }
+          const selectCardtoDiscard = new SelectCard('Select a card to discard', 'Discard', player.cardsInHand, (foundCards: Array<IProjectCard>) => {
+            player.cardsInHand.splice(player.cardsInHand.indexOf(foundCards[0]), 1);
+            player.game.dealer.discard(foundCards[0]);
+            player.game.log('${0} is using their ${1} effect to draw a card.', (b) => b.player(player).card(this));
+            player.game.log('You discarded ${0}', (b) => b.card(foundCards[0]), {reservedFor: player});
+            player.drawCard();
+            return undefined;
+          });
+          selectCardtoDiscard.buttonDanger = true;
+
           return new OrOptions(
-            new SelectOption('Remove a science resource from this card to draw a card', 'Remove resource', () => {
-              player.removeResourceFrom(this);
-              player.drawCard();
-              return undefined;
-            }),
-            new SelectOption('Add a science resource to this card', 'Add resource', () => {
-              player.addResourceTo(this, 1);
+            selectCardtoDiscard,
+            new SelectOption('Do nothing', 'Confirm', () => {
               return undefined;
             }),
           );
         },
-      ), -1); // Unshift that deferred action
+      ));
     }
     return undefined;
   }
